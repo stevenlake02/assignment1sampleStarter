21d2bae857bb36e95441ef012b54e3e06998c66b
steven <122418634@umail.ucc.ie>
2024-11-04 11:58:38 +0000

    Fixed faulty methods in mymath.py

--- mymath.py
+++ mymath.py
@@ -1,14 +1,14 @@
 def add_three(a, b, c):
-    return a-b+c
+    return a+b+c
 
 def subtract_three(a, b, c):
-    return a-b*c
+    return a-b-c
 
 def multiply_three(a, b, c):
-    return a/b*c
-
-def divide_three(a, b, c):
     return a*b*c
 
+def divide_three(a, b, c):
+    return a/b/c
+
 def power(a, b):
-    return a*b
+    return a**b
